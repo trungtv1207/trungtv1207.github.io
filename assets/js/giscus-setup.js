15e63062eaabe8cb71509d3f20f652d9b7fc873b
trungtv1207 <35496172+trungtv1207@users.noreply.github.com>
2026-01-11 13:39:28 +0000

    Deploying to gh-pages from @ trungtv1207/trungtv@c027ed64bd091075547aaf40c32f08ece12d3562 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "trungtv1207/trungtv",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
